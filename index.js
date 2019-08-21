77097d11f6c117ca6df48f3e1476ee4295db4e9f
Di3twater <dev.jonathan.green@gmail.com>
2019-08-21 17:42:28 -0400

    WIP

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var geo = require('./Geo');
 
-const point = {
-    "Latitude": 10,
-    "Longitude": 30,
-}
+geo.geocoder.geocode({ address: '3435 toomer kiln circle', country: 'US', zipcode: '29466' }, function (err, res) {
+    console.log(res);
+    const point = {
+        "Latitude": res[0].latitude,
+        "Longitude": res[0].longitude
+    }
+    console.log(point)
+    const location = geo.getNearest(point)
+    const km = geo.calcCrow(point.Latitude, point.Longitude, location.Latitude, location.Longitude)
+    console.log(location)
+    console.log(km.toFixed(1) + "km")
+    console.log(geo.toMiles(km).toFixed(1) + "m")
 
-const location = geo.getNearest(point)
-const km = geo.calcCrow(point.Latitude,point.Longitude,location.Latitude,location.Longitude)
-console.log(location)
-console.log(/*=========================================*/)
-console.log(km.toFixed(1)+"km")
-console.log(geo.toMiles(km).toFixed(1)+"m")
+});
+
+
