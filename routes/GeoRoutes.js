b92e4f9220d6389defcfa07d2f81ddcfd496b5b9
Di3twater <dev.jonathan.green@gmail.com>
2019-08-21 20:34:05 -0400

    WIP

--- routes/GeoRoutes.js
+++ routes/GeoRoutes.js
@@ -3,7 +3,10 @@ var geoService = require('../Services/GeoService')
 
 exports.closest = async (req, res) => {
     const unit = req.query.unit
-    var data = await geoService.findStore("mi", "3435 toomer kiln circle", "29466")
+    const address = req.query.address
+    const zip = req.query.zip
+
+    var data = await geoService.findStore(unit, address, zip)
     res.json(data);
  };
 
